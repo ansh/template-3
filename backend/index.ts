40304e245a913dda6d8d9ad4554baeb1a69568a1
Ansh Nanda <anshnanda10@gmail.com>
2024-09-07 17:54:01 -0700

    fix ports

--- backend/index.ts
+++ backend/index.ts
@@ -9,7 +9,7 @@ import axios from "axios";
 
 // init express server
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.use(express.json()); // for parsing application/json
 app.use(morgan("dev")); // for pretty logging
 
