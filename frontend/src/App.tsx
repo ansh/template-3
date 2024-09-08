01901ae75f8f34b750b7dc6949889723cff32e82
Ansh Nanda <anshnanda10@gmail.com>
2024-09-07 18:20:14 -0700

    Make components.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,10 @@
 import React from "react";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <div>
-        <h2 className="text-2xl font-semibold text-center border p-4 font-mono rounded-md">
-          Welcome to Template 3
-        </h2>
-      </div>
-      <div className="flex-grow flex items-center justify-center">
-        <h1 className="text-6xl font-bold text-center">
-          Make anything you imagine 🪄
-        </h1>
-      </div>
-      <div></div>
+      <Welcome />
     </main>
   );
 }
---
+++ frontend/src/components/Welcome.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Welcome: React.FC = () => {
+  return (
+    <>
+      <div>
+        <h2 className="text-2xl font-semibold text-center border p-4 font-mono rounded-md">
+          Welcome to Template 3
+        </h2>
+      </div>
+      <div className="flex-grow flex items-center justify-center">
+        <h1 className="text-6xl font-bold text-center">
+          Make anything you imagine 🪄
+        </h1>
+      </div>
+      <div></div>
+    </>
+  );
+};
+
+export default Welcome;
